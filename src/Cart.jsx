88777bdd2f6937387455f9a4d2a1ec11299e7248
Andres Lopez <andreslpzp@gmail.com>
2024-08-15 21:51:35 -0400

    testing cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,43 +1,6 @@
-import React, { useState } from 'react';
-import pizzas from './Pizzas.js'; // Asegúrate de que la ruta sea correcta
-
-function Cart() {
-  const [cartItems, setCartItems] = useState([]);
-
-  const handleAddToCart = (pizza) => {
-    // Lógica para agregar un producto al carrito
-    setCartItems([...cartItems, { ...pizza, quantity: 1 }]);
-  };
-
-  const handleRemoveFromCart = (pizzaId) => {
-    // Lógica para eliminar un producto del carrito
-    setCartItems(cartItems.filter(item => item.id !== pizzaId));
-  };
-
-  const calculateTotal = () => {
-    // Lógica para calcular el total del carrito
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-  };
-
+function Cart({ pizza }) {
   return (
-    <div>
-      <h1>Carrito de Compras</h1>
-      {cartItems.length === 0 ? (
-        <p>Tu carrito está vacío.</p>
-      ) : (
-        <div>
-          {cartItems.map(item => (
-            <div key={item.id}>
-              {/* Aquí renderizarías los detalles de cada pizza en el carrito */}
-              <p>{item.name}</p>
-              <p>Cantidad: {item.quantity}</p>
-              <button onClick={() => handleRemoveFromCart(item.id)}>Eliminar</button>
-            </div>
-          ))}
-          <p>Total: ${calculateTotal()}</p>
-        </div>
-      )}
-    </div>
+    <p>esto es una prueba</p>
   );
 }
 
