0e7c1ad569f6845f9fa2ef8e245f9d738d1978f7
Andres Lopez <andreslpzp@gmail.com>
2024-08-25 00:35:30 -0400

    Updated info in cards, added description

--- frontend/src/Pizza.jsx
+++ frontend/src/Pizza.jsx
@@ -2,30 +2,31 @@ import React from 'react';
 import './assets/CSS/Pizza.css';
 
 const Pizza = ({ pizza }) => {
-  if (!pizza) {
-    return <p>Cargando información de la pizza...</p>;
-  }
+    if (!pizza) {
+        return <p>Cargando información de la pizza...</p>;
+    }
 
-  return (
-    <div className="pizza-detail">
-      <h1 className="pizza-name">{pizza.name}</h1>
-      <img src={pizza.img} alt={pizza.name} className="pizza-image" />
-      <p className="pizza-price">Precio: ${pizza.price.toFixed(2)}</p>
-      <div className="pizza-ingredients">
+    return (
+        <div className="pizza-detail">
+            <h1 className="pizza-name">{pizza.name}</h1>
+            <img src={pizza.img} alt={pizza.name} className="pizza-image" />
+            <p className="pizza-price">Precio: ${pizza.price.toFixed(2)}</p>
+            <div className="pizza-ingredients">
         <strong>Ingredientes:</strong>
         <ul>
-          {pizza.ingredients.map((ingredient, index) => (
-            <li key={index}>{ingredient}</li>
-          ))}
+            {pizza.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+            ))}
         </ul>
-      </div>
-      <p className="pizza-description">{pizza.description}</p>
-      <button className="add-to-cart-button">Añadir al carrito</button>
-    </div>
-  );
+        </div>
+            <p className="pizza-description">{pizza.desc}</p>
+            <button className="add-to-cart-button">Añadir al carrito</button>
+        </div>
+    );
 };
 
 export default Pizza;
+
 
 
 
